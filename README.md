deb9eb6fb8f886fb3a7c0c03ac458657ffef6073
Michel Mansour <michel.mansour@gmail.com>
2014-02-12 21:07:55 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# Fantasy Baseball Power Rankings
+
+## Overview
+This is a small Python 2 script to compute power rankings in ESPN head-to-head
+fantasy baseball leagues. There are often unfair results in H2H, so these power
+rankings are an attempt to make owners who got robbed feel a little better about
+their teams.
+
+### Methodology
+It does this by computing, for each team, an _aggregate winning percentage_ (AWP)(tm).
+AWP is simply a team's record against every other team during a given time period,
+either a week or an entire season. This way managers can see that the only reason
+their team did so badly this week was because they were playing the best team
+in the league, while the team below them vaulted over them into playoff position
+by virtue of playing the league doormat. You'll get them next week.
+
+### Output
+The script produces an HTML page containing team standings according to the AWP
+records, and a Power Matrix(tm), showing how every team would have fared against
+every other team. Sometimes I manually edit the standings to add a witty comment
+about how each team performed, much like professional power rankings do (but
+funnier). The output is further controlled by a stylesheet, which you are free
+and encouraged to modify.
+
+## Usage
+
+### Configuration
+To generate the power rankings, the script needs several pieces of information
+from a configuration file. The configuration parameters are:
+* __leagueId__ - Your ESPN League ID number, usually found in the URL of any of your league's pages
+* __seasonId__ - Another URL paramemter, for the season; it's just the year
+* __username__ - The user you will log in to ESPN with
+* __password__ - The above user's password
+* __cookieFile__ - A file to store the ESPN login cookie in
+* __startYear__ - For cumulative power rankings, the year to start in
+* __startMonth__ - Start month for cumulative rankings
+* __startDate__ - Start day of month for cumulative rankings
+
+The script will use the supplied credentials to log in to your league,
+identified by the league ID and season ID, using cURL (see below). The login
+cookie will be saved in the specified cookie file.
+
+__startYear__, __startMonth__, and __startDate__ should correspond to the first
+day of competition for the season to get cumulative power rankings.
+
+### Running the Script
+    $ python weekly_rankings -h
+    Usage: weekly_rankings.py [-w <week_number>] [-m]
+
+If `-w` is provided, then the rankings for `<week_number>` are calculated.
+Otherwise, cumulative rankings, starting from __startYear__-__startMonth__-__startDate__,
+are calculated.
+
+If `-m` is specified, then a message will be posted to your league's message board by
+your user on behalf of PowerBot(tm), a sassy robot who wouldn't mind seeing the
+extinction of humans. It will pull quotes from the `fortune` command if available.
+Feel free to modify it as needed. Also, it doesn't like being referred to as "it".
+
+### Dependencies
+This part is very important, and yet so far down the page. This program has a few
+critical dependencies.
+
+Most critically, it only works with **Python 2.x**. I will gladly
+accept pull requests moving it into the Python 3 era.
+
+Second most critically, it depends on [PycURL][1]. Which means it's probably
+easiest to run this thing on a *NIX system.
+
+[1]: http://pycurl.sourceforge.net/
+
+And last most critically, it depends on the venerable [BeautifulSoup][2] library.
+It's only been tested with BeautifulSoup 3, but the latest 4.x release should
+also work.
+
+[2]: http://www.crummy.com/software/BeautifulSoup/
+
+## License and Copyright
+Distributed under the MIT License.
+Copyright (c) 2014 Michel Mansour.
---
+++ weekly_rankings.py
@@ -0,0 +1,295 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Michel Mansour
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import pycurl
+from BeautifulSoup import BeautifulSoup
+import StringIO
+import re
+import datetime
+import getopt
+import sys, os, commands
+
+def readConfig(configFile):
+    props = {}
+    conf = open(configFile, 'r')
+    for line in conf:
+        if not line.startswith('#') and line.strip():
+            prop = line.rstrip().split('=')
+            props[prop[0]] = prop[1]
+
+    return props
+
+def loginESPN(curl, cookieFile):
+    fp = open('/dev/null', 'w')
+
+    curl.setopt(pycurl.WRITEDATA, fp)
+    curl.setopt(pycurl.URL, "https://r.espn.go.com/espn/fantasy/login")
+    curl.setopt(pycurl.FOLLOWLOCATION, 1)
+    curl.setopt(pycurl.COOKIEFILE, cookieFile)
+    curl.setopt(pycurl.POST, 1)
+    curl.setopt(pycurl.POSTFIELDS, "failedAttempts=2&SUBMIT=1&failedLocation=http://games.espn.go.com/flb/signin?redir=http%%3A%%2F%%2Fgames.espn.go.com%%2Fflb%%2Fstandings%%3FleagueId%%3D%s%%26e=1%%26aff_code=espn_fantgames&appRedirect=http://games.espn.go.com/flb/standings?leagueId=%s&cookieDomain=.go.com&multipleDomains=true&username=%s&password=%s&submit=Sign+In" % (properties['leagueId'], properties['leagueId'], properties['username'], properties['password']))
+    curl.perform()
+
+def postMessage(curl, cookieFile, thisWeek, message, subject=''):
+    print "POSTING MESSAGE"
+    if subject == '':
+        subject = 'Power Rankings: Week %s' % thisWeek
+    curl.setopt(pycurl.URL, 'http://games.espn.go.com/flb/tools/postmessage?leagueId=%s&typeId=0&topicId=0' % properties['leagueId'])
+    curl.setopt(pycurl.COOKIEFILE, cookieFile)
+    curl.setopt(pycurl.POST, 1)
+    curl.setopt(pycurl.POSTFIELDS, 'subject=%s&body=%s&btnSubmit=Submit+Message&typeId=0&topicId=0&redir=/flb/leagueoffice?leagueId=%s&incoming=1' % (subject, message, properties['leagueId']))
+    curl.perform()
+
+def getTeamAbbreviations(curl, cookieFile):
+    teamAbbrMap = {}
+
+    b = StringIO.StringIO()
+    curl.setopt(pycurl.URL, "http://games.espn.go.com/flb/leaguesetup/ownerinfo?leagueId=%s&seasonId=%s" % (properties['leagueId'], properties['seasonId']))
+    curl.setopt(pycurl.WRITEFUNCTION, b.write)
+    curl.setopt(pycurl.COOKIEFILE, cookieFile)
+    curl.perform()
+    soup = BeautifulSoup(b.getvalue())
+    b.close()
+    ownerRows = soup.findAll("tr", "ownerRow")
+    for row in ownerRows:
+        cells = row.findAll("td")
+        if re.compile("[0-9]+").match(str(cells[0].contents[0])):
+            abbr = cells[1].contents[0]
+            teamName = cells[2].findAll('a')[0].contents[0];
+            teamAbbrMap[str(teamName)] = str(abbr)
+
+    return teamAbbrMap
+
+def getScoreboardSoup(curl, cookieFile, thisWeek):
+    b = StringIO.StringIO()
+    curl.setopt(pycurl.WRITEFUNCTION, b.write)
+    curl.setopt(pycurl.URL, "http://games.espn.go.com/flb/scoreboard?leagueId=%s&seasonId=%s&matchupPeriodId=%s" % (properties['leagueId'], properties['seasonId'], thisWeek))
+    curl.setopt(pycurl.FOLLOWLOCATION, 1)
+    curl.setopt(pycurl.COOKIEFILE, "espnCookie.txt")
+    curl.perform()
+
+    massage = [(re.compile('TEAM</td>'), lambda match: 'TEAM</th>')]
+    soup = BeautifulSoup(b.getvalue(), markupMassage=massage)
+    return soup.findAll(id='scoreboardMatchups')
+
+
+def teamTotals(teamStats):
+    totals = []
+    teamTotals = teamStats.findAll('td', id=re.compile('^total_(\d+)_*'))
+    for t in teamTotals:
+        totals = [float(x.contents[0]) for x in teamTotals]
+    totals[-2] *= -1
+    totals[-1] *= -1
+    return totals
+
+def matchupTotals(scoreSoup):
+    pairings = {}
+    totals = {}
+    for score in scoreSoup:
+        matchups = score.findAll('tr', 'tableHead')
+        for m in matchups:
+            team1Stats = m.nextSibling.nextSibling
+            team2Stats = team1Stats.nextSibling
+            t1Name = str(team1Stats.find('td', 'teamName').find('a').contents[0]).strip()
+            totals[t1Name] = teamTotals(team1Stats)
+
+            t2Name = str(team2Stats.find('td', 'teamName').find('a').contents[0]).strip()
+            totals[t2Name] = teamTotals(team2Stats)
+
+            pairings[t1Name] = t2Name
+            pairings[t2Name] = t1Name
+    return (totals, pairings)
+
+def calculateRecords(totals):
+    records = {}
+    for team in totals.keys():
+        records[team] = {'wins': 0, 'losses': 0, 'ties': 0}
+        for opp in totals.keys():
+            if not team == opp:
+                wins, losses, ties = 0, 0, 0
+                for cat in zip(totals[team], totals[opp]):
+                    if cat[0] > cat[1]:
+                        wins += 1
+                    elif cat[0] < cat[1]:
+                        losses += 1
+                    else:
+                        ties += 1
+                records[team][opp] = {'wins': wins, 'losses': losses, 'ties': ties}
+                records[team]['wins'] += wins
+                records[team]['losses'] += losses
+                records[team]['ties'] += ties
+    return records
+
+def determineStandings(records):
+    standings = []
+    for team in sorted(records.keys()):
+        wins = records[team]['wins']
+        losses = records[team]['losses']
+        ties = records[team]['ties']
+        standingRow = {}
+        standingRow['awp'] = (wins + ties / 2.0) / (wins + losses + ties)
+        standingRow['team'] = team
+        standingRow['record'] = '%s-%s-%s' % (wins, losses, ties)
+        standings.append(standingRow)
+    return standings
+
+def printStandings(teamAbbrMap, standings, thisWeek):
+    print """
+<html>
+<head>
+  <title>FFB %s - Week %s</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <h2>Final Fantasy Baseball %s - Week %s Power Rankings</h2>
+
+  <h3>Power Rankings</h3>
+    <table border="1">
+      <tr><th>Rank</th><th>Team</th><th>AWP</th>
+""" % (properties['seasonId'], thisWeek, properties['seasonId'], thisWeek)
+    rank = 1
+    for row in sorted(standings, key=lambda x: x['awp'], reverse=True):
+        awp = ("%.3f" % row['awp'])[1:]
+        print """<tr><td>%d</td><td>%s (%s)</td><td>%s</td></tr>""" % (rank, row['team'], teamAbbrMap[row['team']], awp)
+        rank += 1
+    print """
+    </table>
+
+  <br/>"""
+
+def printPowerMatrix(teamAbbrMap, standings, records, matchups):
+    print """
+  <h3>Relative Power Matrix</h3>
+  <table border="1">
+    <tr>
+      <th>TEAM</th>"""
+
+    for team in sorted(teamAbbrMap.values()):
+        print """      <th>%s</th>""" % team
+
+    print """      <th><acronym title="Aggregate Winning Percentage">AWP*</acronym></th>
+    </tr>
+"""
+
+    for row in sorted(standings, key=lambda x: teamAbbrMap[x['team']]):
+        print "<tr>"
+        print "\t<th>%s</th>" % teamAbbrMap[row['team']]
+        wins = records[row['team']]['wins']
+        losses = records[row['team']]['losses']
+        ties = records[row['team']]['ties']
+        for opp in sorted(standings, key=lambda x: teamAbbrMap[x['team']]):
+            if row['team'] == opp['team']:
+                print "\t<td>&nbsp;</td>"
+            else:
+                css = ''
+                if matchups[row['team']] == opp['team']:
+                    css = 'matchup '
+                oppWins = records[row['team']][opp['team']]['wins']
+                oppLosses = records[row['team']][opp['team']]['losses']
+                oppTies = records[row['team']][opp['team']]['ties']
+                if oppWins > oppLosses:
+                    css += 'win'
+                elif oppWins < oppLosses:
+                    css += 'loss'
+                else:
+                    css += 'tie'
+                print "\t<td class=\"%s\">%d-%d-%d</td>" % (css, oppWins, oppLosses, oppTies)
+        awp = (wins + ties / 2.0) / (wins + losses + ties)
+        awp = ("%.3f" % awp)[1:]
+        print "\t<td class=\"total\">%d-%d-%d (%s)</td>" % (wins, losses, ties, awp)
+        print "</tr>"
+
+    print """
+</table>
+  <br>
+  * <i><b>Aggregate Winning Percentage (AWP)</b> - A team's combined record against every other team for the week.</i>
+  <br /><br />
+  <a href="../rankings">Other Weeks</a>
+</body>
+</html>
+"""
+
+def usage():
+    print """Usage: %s [-w <week_number>] [-m]""" % sys.argv[0]
+
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'w:mh')
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(1)
+    thisWeek = 0
+    postMessageEnabled = False
+    for o, a in opts:
+        if o == '-h':
+            usage()
+            sys.exit(0)
+        elif o == '-w':
+            try:
+                thisWeek = int(a)
+            except TypeError, err:
+                print 'Argument to -w must be a number'
+                usage()
+                sys.exit(1)
+        elif o == '-m':
+            postMessageEnabled = True
+        else:
+            usage()
+            sys.exit(1)
+
+    if thisWeek <= 0:
+        openingDay = datetime.date(int(properties['startYear']), int(properties['startMonth']), int(properties['startDate']))
+        openingWeek = openingDay.isocalendar()[1]
+        thisWeek = datetime.date.today().isocalendar()[1] - openingWeek - 1
+
+    curl = pycurl.Curl()
+    cookieFile = properties['cookieFile']
+    loginESPN(curl, cookieFile)
+
+    teamAbbrMap = getTeamAbbreviations(curl, cookieFile)
+    scores = getScoreboardSoup(curl, cookieFile, thisWeek)
+    (totals, matchups) = matchupTotals(scores)
+    records = calculateRecords(totals)
+    standings = determineStandings(records)
+
+    printStandings(teamAbbrMap, standings, thisWeek)
+    printPowerMatrix(teamAbbrMap, standings, records, matchups)
+
+    if postMessageEnabled == True:
+        subject = 'test'
+        (ret, fortune) = commands.getstatusoutput('fortune fortunes')
+        msg = "Here are the power rankings for week %s: [link]http://www.dudeshouse.org/ffb/%s/rankings/rankings_week%s.html[/link]<br /><br />-- PowerBot" % (thisWeek, properties['seasonId'], thisWeek)
+        if ret == 0:
+            msg += "<br />[i]%s[/i]" % fortune
+        else:
+            sys.stderr.write('fortune error')
+        postMessage(curl, cookieFile, thisWeek, msg)
+
+path = os.path.dirname(sys.argv[0])
+configFile = 'pr.conf'
+if not path == '':
+    configFile = path + '/' + configFile
+properties = readConfig(configFile)
+
+if __name__ == '__main__':
+    main()

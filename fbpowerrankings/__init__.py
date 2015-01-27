8e5f99aa72079cb2fc8f187f417e66eb45565d71
Michel Mansour <michel.mansour@gmail.com>
2015-01-27 11:35:48 -0500

    Create fbpowerrankings package.
    - Organize files into conventional package structure.
    - Add setup.py for installation via setuptools.
    - Add __init__.py to package directory to export classes at top package level.

---
+++ fbpowerrankings/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'fbpowerrankings'
+__version__ = '0.1'
+__author__ = 'Michel Mansour'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Michel Mansour'
+
+from .power_rankings import SeasonRankings, WeeklyRankings
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='fbpowerrankings',
+      version='0.1',
+      description='Fantasy baseball power rankings',
+      url='http://github.com/michelmansour/fb-power-rankings',
+      author='Michel Mansour',
+      license='MIT',
+      package=['fbpowerrankings'],
+      install_requires=['requests>=2.5.1',
+                        'lxml>=3.4.1',
+                        'beautifulsoup4>=4.3.2'],
+      zip_safe=False)
